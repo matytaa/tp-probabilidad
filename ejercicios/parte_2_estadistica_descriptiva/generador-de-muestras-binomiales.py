1dc1d2a0645b8571e42cda02e72c4090c6de7676
Matias Nievas Soto <matias.nievas@etermax.com>
2020-11-28 21:04:26 -0300

    Fix ejercicio 2.3

--- ejercicios/parte_2_estadistica_descriptiva/generador-de-muestras-binomiales.py
+++ ejercicios/parte_2_estadistica_descriptiva/generador-de-muestras-binomiales.py
@@ -1,30 +1,22 @@
 import collections
-import matplotlib.pyplot as plt
-from statsmodels.distributions.empirical_distribution import ECDF
 from ejercicios.parte_1_simulacion.binomial import fn_binomial_array
 # # Ejercicio 3
 # Generar una muestra de números Bin(10, 0.3) de tamaño de muestra N = 50.
 # Construir la función de distribución empírica de dicha muestra.
 
-def función_de_distribución_acumulativa_empirica(casos, n, p):
-    val_e3 = fn_binomial_array(casos, n, p)
-    print(val_e3)
-    collections.Counter(val_e3)
-    ecdf = ECDF(val_e3)
-    print('P(x<0): %.3f' % ecdf(0))
-    print('P(x<1): %.3f' % ecdf(1))
-    print('P(x<2): %.3f' % ecdf(2))
-    print('P(x<3): %.3f' % ecdf(3))
-    print('P(x<4): %.3f' % ecdf(4))
-    print('P(x<5): %.3f' % ecdf(5))
-    print('P(x<6): %.3f' % ecdf(6))
+def función_de_distribución_acumulativa_empirica(array_de_valores, limite_superior):
+    mapa_de_valores = collections.Counter(array_de_valores)
+    keys = sorted(mapa_de_valores.keys())
+    suma = 0
+    for i in range(0, len(keys)):
+       if(keys[i] <= limite_superior):
+           suma += mapa_de_valores[keys[i]]
+    return suma/ len(array_de_valores)
 
-    plt.figure("Muestra de números Bin(10, 0.3) de tamaño de muestra N = 50")
-    plt.plot(ecdf.x, ecdf.y)
-    plt.show()
-
-
+valor_limite = 5
 casos = 50
 n = 10
 p = 0.3
-función_de_distribución_acumulativa_empirica(casos, n, p)
+valores_e3 = fn_binomial_array(casos, n, p)
+resultado = función_de_distribución_acumulativa_empirica(valores_e3, valor_limite)
+print(resultado)
