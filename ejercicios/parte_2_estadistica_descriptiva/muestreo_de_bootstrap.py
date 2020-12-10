d01558924882aa441d21f686aebdc391753b4377
Matias Nievas Soto <matias.nievas@etermax.com>
2020-12-09 21:50:56 -0300

    Corrijo boostrap

--- ejercicios/parte_2_estadistica_descriptiva/muestreo_de_bootstrap.py
+++ ejercicios/parte_2_estadistica_descriptiva/muestreo_de_bootstrap.py
@@ -1,16 +1,15 @@
-import numpy as np
+from random import choices
+from funciones.funciones import media_muestral
 
 
 def generar_muestra_segun_cdf(distribucion_empirica, casos):
-    muestra = []
+    medias = []
+    largo = len(distribucion_empirica)
+    valores = []
+    for i in range(0, largo):
+        valores.append(distribucion_empirica[i][1])
     for i in range(0, casos):
-        muestra.append(np.random.uniform(0, 1))
-
-    muestra_nueva = []
-    for i in range(0, casos):
-        valor = muestra[i]
-        for j in (range(0, len(distribucion_empirica))):
-            if (distribucion_empirica[j][1] >= valor):
-                muestra_nueva.append(distribucion_empirica[j][0])
-                break
-    return muestra_nueva
+        muestra_nueva = choices(valores, k=largo)
+        media = media_muestral(muestra_nueva)
+        medias.append(media)
+    return medias
