2f1ec2da051976e2def287f906e310997e49546d
Matias Nievas Soto <matias.nievas@etermax.com>
2020-12-10 14:05:44 -0300

    Ajusto normalizacion

--- ejercicios/parte_3_convergencia/normalizacion.py
+++ ejercicios/parte_3_convergencia/normalizacion.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
+import math
 
 def normalizar(muestra_a_normalizar, media_muestral, varianza):
     normal = np.zeros(len(muestra_a_normalizar))
     for i in range(0, len(muestra_a_normalizar)):
         valor = muestra_a_normalizar[i]
         calculo_a = valor - media_muestral
-        calculo = calculo_a / varianza
+        calculo = calculo_a / math.sqrt(varianza)
         normal[i] = calculo
 
     df = pd.DataFrame({"valor": muestra_a_normalizar, "estandarizado": normal})
